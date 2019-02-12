ffa037bc03ebb8e07698dee5fad4a74f3856bfe7
hanfuyin <18910490095@163.com>
2019-02-12 10:16:38 +0000

    添加验证码

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,6 +40,22 @@
                         </div>
 
                         <div class="form-group row">
+                          <label for="captcha" class="col-md-4 col-form-label text-md-right">验证码</label>
+
+                          <div class="col-md-6">
+                            <input id="captcha" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}" name="captcha" required>
+
+                            <img class="thumbnail captcha mt-3 mb-2" src="{{ captcha_src('flat') }}" onclick="this.src='/captcha/flat?'+Math.random()" title="点击图片重新获取验证码">
+
+                            @if ($errors->has('captcha'))
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $errors->first('captcha') }}</strong>
+                                </span>
+                            @endif
+                          </div>
+                        </div>
+
+                        <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
